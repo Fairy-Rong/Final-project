56813a27496f7e0dd00e40ea527035ba39398fdb
Fairy-Rong <707305810@qq.com>
2022-11-03 18:59:11 +0800

    Update crimes_time_app.py

--- apps/crimes_time_app.py
+++ apps/crimes_time_app.py
@@ -86,6 +86,8 @@ def app():
     st.image(image, caption='Hell is empty. The devil is on Chicago.')
 
     # Artificial Intelligence Dialogue
+    st.write('')
+    st.write('')
     st.subheader('Artificial Intelligence Dialogue')
     user_inputs = st.text_input('Your question: ')
     st.write('')
